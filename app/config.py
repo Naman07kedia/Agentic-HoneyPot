4f4d6069f5d7cc114ba2a8d4fea24bab64c08f37
Naman Kedia <149782261+Naman07kedia@users.noreply.github.com>
2026-02-04 18:21:23 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,22 @@
-import os
+'''import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 API_KEY = os.getenv("HONEY_POT_API_KEY")
 
-print("DEBUG: config.py loaded, API_KEY =", API_KEY)
+print("DEBUG: config.py loaded, API_KEY =", API_KEY)'''
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("HONEY_POT_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+GUVI_CALLBACK_URL = "https://hackathon.guvi.in/api/updateHoneyPotFinalResult"
+
+MAX_SESSIONS = 5000
+CALLBACK_MIN_TURNS = 6
+
